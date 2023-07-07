d919e5125bd09ccd0c15c80a3c1f17d84bbe2044
Ode Ardika <dirfanardika@gmail.com>
2023-07-07 23:06:05 +0800

    save data from regis to database

--- register.php
+++ register.php
@@ -2,38 +2,67 @@
 include("conn.php");
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Retrieve form data
-    $name = $_POST['name'];
     $nim = $_POST['nim'];
     $email = $_POST['email'];
+    $name = $_POST['name'];
+    $username =  $_POST['username'];
     $password = $_POST['password'];
-    $class = $_POST['class'];
-    $angkatan = $_POST['angkatan'];
-    // Retrieve other form fields based on the category
+    // Apakah NIM sudah ada di DB
+    $query = "SELECT * FROM user WHERE id = '$nim'";
+    if(mysqli_num_rows(mysqli_query($conn,$query)) !== 0){
+        gagal_regis('NIM Sudah Terdaftar');
+    }
+    // Apakah Email sudah ada di DB
+    $query = "SELECT * FROM user WHERE email = '$email'";
+    if(mysqli_num_rows(mysqli_query($conn,$query)) !== 0){
+        gagal_regis('Email Sudah Terdaftar');
+    }
 
-    // cek apakah email sudah digunakan
+    $role = $_POST['category'];
+    if($role === 'mahasiswa'){
+        // Menambahkan User
+        add_user_table($conn, $name,$username,$nim,$email,$password,$role);
+        // Memeriksa apakah kelas sudah benar
+        $class = $_POST['class'];
+        $query = "SELECT * FROM kelas WHERE nama_kode_kelas = '$class'";
+        $class_row = mysqli_query($conn,$query);
+        if(mysqli_num_rows($class_row) === 0){
+            gagal_regis('Kelas tidak ada');
+        }
+        $class_row = mysqli_fetch_assoc($class_row);
+        $class_id = $class_row['id'];
 
-    // Insert data into the database
-    // Replace 'mahasiswa' with your actual table name
+        // Menambahkan jumlah mahasiswa
+        // Menambahkan data di tabel mahasiswa
+        $query = "INSERT INTO mahasiswa (id_user, kelas) VALUES ($nim,$class_id)";
+        mysqli_query($conn,$query);
+    }
+    // Retrieve form data
     
-    // full
-    // $query = "INSERT INTO mahasiswa (nama, nim, email, password, kelas_id, angkatan_id) 
-    // VALUES ('$name', '$nim', '$email', '$password', '$class', '$angkatan')";
+    
+    
+    
+    
+    // Redirect to a success page or display a success message
+    header('Location: index.html'); // Replace 'success.html' with your actual success page
+    exit;
+}
 
-    // demo
-    $query = "INSERT INTO mahasiswa (nama, nim, email, password) 
-    VALUES ('$name', '$nim', '$email', '$password')";
+function gagal_regis($msg_error){
+    header('Location: registration_form.php?error='.$msg_error);
+    exit;
+}
+
+function add_user_table($conn, $name, $username, $nim,$email,$password,$role){
+    $query = "INSERT INTO user (fullname, id, email, password,role,username) 
+    VALUES ('$name', $nim, '$email', '$password','$role','$username')";
     
     if (mysqli_query($conn, $query)) {
         echo "New record created successfully";
     } else {
         echo "Error: " . $query . "<br>" . mysqli_error($conn);
     }
-    
-    mysqli_close($conn);
+    // mysqli_close($conn);
 
-    // Redirect to a success page or display a success message
-    header('Location: index.html'); // Replace 'success.html' with your actual success page
-    exit;
 }
 ?>
